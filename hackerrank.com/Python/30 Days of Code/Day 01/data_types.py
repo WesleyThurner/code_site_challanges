ace73287ef8e7f7a619b1415ea37ba53181d21b3
WesleyT <wesleythurner@gmail.com>
2019-03-14 17:13:29 -0400

    hackerrank

---
+++ hackerrank.com/Python/30 Days of Code/Day 01/data_types.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+i2 = 8
+d2 = 8.0
+s2 = 'CodeChallenge '
+# Read and save an integer, double, and String to your variables.
+i2 = input()
+d2 = input()
+s2 = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + int(i2))
+# Print the sum of the double variables on a new line.
+print(d + float(d2))
+# Concatenate and print the String variables on a new line
+print(s + s2)
+# The 's' variable above should be printed first.
+
